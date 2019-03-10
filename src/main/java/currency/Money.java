83f896c089c06e3fa93bc124df1ffea1cc468611
Wiktor Rup <wiktor-rup@wp.pl>
2019-03-10 08:29:17 +0100

    removing unnecessary classes and test

--- src/main/java/currency/Money.java
+++ src/main/java/currency/Money.java
@@ -9,12 +9,12 @@ public class Money {
         this.currency = currency;
     }
 
-    public static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
     }
 
-    public static Frank frank(int amount) {
-        return new Frank(amount, "CHF");
+    public static Money frank(int amount) {
+        return new Money(amount, "CHF");
     }
 
     public Money times(int multiplier) {
--- src/test/java/currencyTests/testCurrency.java
+++ src/test/java/currencyTests/testCurrency.java
@@ -1,6 +1,5 @@
 package currencyTests;
 
-import currency.Frank;
 import currency.Money;
 import org.junit.jupiter.api.Test;
 
@@ -10,39 +9,24 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class testCurrency {
 
     @Test
-    public void testMultiplication() {
+    void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
-    public void testEquality() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(6), Money.dollar(5));
-        assertEquals(Money.frank(5), Money.frank(5));
-        assertNotEquals(Money.frank(5), Money.frank(6));
         assertNotEquals(Money.frank(5), Money.dollar(5));
 
 
     }
 
     @Test
-    public void testFrankMultiplication() {
-        Frank five = Money.frank(5);
-        assertEquals(Money.frank(10), five.times(2));
-        assertEquals(Money.frank(15), five.times(3));
-    }
-
-    @Test
     void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.frank(1).currency());
-    }
-
-    @Test
-    void testDifferentClassEquality() {
-        assertEquals(new Money(10, "CHF"), new Frank(10, "CHF"));
-
     }
 }
